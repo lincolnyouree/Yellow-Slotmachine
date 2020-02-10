93f02b35b0866b05c7be0ba07de1c3c1f609740b
Lincoln Youree <lincolnyouree@Lincolns-MacBook-Air.local>
2020-02-10 12:25:52 -0600

    logic works

--- js/main.js
+++ js/main.js
@@ -1,61 +1,128 @@
 /*----- constants -----*/
 
-const board = [];
+const money = 100;
+let field = [];
 const bet = 10;
 
 /*----- app's state (variables) -----*/
 
-
 /*----- cached element references -----*/
 
-let slot1 = [];
-let slot2 = [];
-let slot3 = [];
+const message = document.getElementById('message');
+
+slot1 = document.getElementById('slotOne');
+slot2 = document.getElementById('slotTwo');
+slot3 = document.getElementById('slotThree');
+betAmount = document.getElementById('money');
 
 /*----- event listeners -----*/
 
-document.getElementById('btn').addEventListener('click', click1);
-document.getElementById('btn').addEventListener('click', click2);
-document.getElementById('btn').addEventListener('click', click3);
+document.getElementById('btn').addEventListener('click', handleClick);
+// document.getElementById('btn').addEventListener('click', click2);
+// document.getElementById('btn').addEventListener('click', click3);
 
 /*----- functions -----*/
 
+// function render() {
+// 	renderHands();
+// 	renderControls();
+// 	if (winner) {
+// 		renderWinnerMessage();
+// 	} else {
+// 		renderTurnMessage();
+// 	}
+// }
 
-function click1() {
-    let randNum = Math.floor((Math.random() * 3)) + 1;
-    document.getElementById('slotOne').value = randNum; 
-    slot1.push(randNum);
-    randomNum();
-    checkWinner();
+
+
+// initializeSlots();
+// initializeMoney();
+
+// function initializeSlots() {
+//     board = ['null', 'null', 'null'];
+//     message.textContent = "Spin!";
+// }
+
+// function initializeMoney() {
+//     $("#money").text("$" + " " + money);
+// }
+
+// function spin() {
+//     let randNum = Math.floor((Math.random() * 3)) + 1;
+//     document.getElementById('slotOne', 'slotTwo', 'slotThree').value = randNum; 
+//     slot1.push(randNum);
+//     randomNum();
+//     checkWinner();
+// }
+
+function checkWinner() {
+    if (field[0] === field[1] && field[2] === field[1]) {
+        console.log('WIN WORKS');
+        message.textContent = 'Congratulations';
+    } 
+    else {
+        message.textContent = 'Game Over!';
+    }
+    
 }
 
+// function jackpot() {
+//     message.textContent = "Congratulations!";
+// }
 
-function click2() {
-    let randNum = Math.floor((Math.random() * 3)) + 1;
-    document.getElementById('slotTwo').value = randNum; 
-    slot2.push(randNum);
-    randomNum();
-    checkWinner();
-}
-
-
-function click3() {
-    let randNum = Math.floor((Math.random() * 3)) + 1;
-    document.getElementById('slotThree').value = randNum; 
-    slot3.push(randNum);
-    randomNum();
-    checkWinner();
-}
+// function gameOver() {
+//     message.textContent = "Game Over!";
+// }
 
 // function checkWinner() {
-//     if ('slotOne' ==== 'slotTwo' === 'slotThree') {
+//     if (slot1 ==== 1 && slot2 === 1 && slot3 === 1) {
 //         message.textContent = "Congratulations!";
-//         winner = true;
+//         jackpot = true;
+//     } if (slot1 ==== 2 && slot2 === 2 && slot3 === 2) {
+//         message.textContent = "Congratulations!";
+//         jackpot = true;
+//     } if (slot1 ==== 3 && slot2 === 3 && slot3 === 3) {
+//         message.textContent = "Congratulations!";
+//         jackpot = true;
 //     } else {
-//         message.textContent = "Game Over!";
-//         winner = false;
+//         return gameOver();
 //     }
 // }
+
+
+
+function handleClick() {
+    let randNum = Math.floor((Math.random() * 3)) + 1;
+    document.getElementById('slotOne').value = randNum; 
+    field[0]=(randNum);
+    let randNum2 = Math.floor((Math.random() * 3)) + 1;
+    document.getElementById('slotTwo').value = randNum2; 
+    field[1]=(randNum2);
+    let randNum3 = Math.floor((Math.random() * 3)) + 1;
+    document.getElementById('slotThree').value = randNum3; 
+    field[2]=(randNum3);
+    checkWinner();
+}
+
+// function click2() {
+//     let randNum = Math.floor((Math.random() * 3)) + 1;
+//     document.getElementById('slotTwo').value = randNum; 
+//     slot2.push(randNum);
+//     randomNum();
+//     checkWinner();
+// }
+
+// function click3() {
+//     let randNum = Math.floor((Math.random() * 3)) + 1;
+//     document.getElementById('slotThree').value = randNum; 
+//     slot3.push(randNum);
+//     randomNum();
+//     checkWinner();
+// }
+
+
+
+
 
 
 
@@ -71,31 +138,6 @@ function click3() {
 // document.getElementById('countdown').innerHTML = timeLeft + ' seconds remaining.'
 // timeLeft -= 1; // makes it count down infinitely
 // if(timeLeft < 0) {
-//     document.getElementById('countdown').innerHTML = 'Finished!'
+//     document.getElementById('countdown').innerHTML = 'LET'S GO!'
 // }}, 1000); // , 5000 sets 5 second delay - putting nothing implies 0 seconds.
-
-
-
-// OTHER IDEAS
-
-
-/*----- constants -----*/
-
-// const money = 100;
-// const board = [];
-// const bet = 10; 
-
-
-// function spin() {
-//     Math.random
-//     checkForWinner()
-// }
-
-// function checkForWinner() {
-//     if (x === y === z = turkey picture) {
-//         money += bet * turkey multiplier
-//     } else {
-//         "you suck"
-//     }
-// } 
 
